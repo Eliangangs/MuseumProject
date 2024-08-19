aa408cfe368972388869fd3bc094650cc14902a9
Eliangangs <eliangangs@gmail.com>
2024-08-19 11:05:05 +0300

    Видалила папочку з зображеннями під кнопки, адобтивниій файл і всі силки на нього, додала кнопки

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+var animateButton = function(e) {
+
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove('animate');
+  
+  e.target.classList.add('animate');
+  setTimeout(function(){
+    e.target.classList.remove('animate');
+  },700);
+};
+
+var bubblyButtons = document.getElementsByClassName("bubbly-button");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener('click', animateButton, false);
+}
+
 const title = document.getElementsByClassName ('title__stationary');
 const text = document.getElementsByClassName ('text_inf__stationary');
 
@@ -69,3 +87,4 @@ function toChangeText (number) {
       break;
   }
 }
+
